7d77609e71915d627903898ca0584c25dbf59f0a
Aishwarya Maitra <maitraaishwarya@gmail.com>
2023-03-24 00:30:04 +0530

    Initial Commit

---
+++ history.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Wed Feb  8 17:16:34 2023)---
+runfile('C:/Users/Aishwarya/.spyder-py3/temp.py', wdir='C:/Users/Aishwarya/.spyder-py3')
+runcell(0, 'C:/Users/Aishwarya/.spyder-py3/temp.py')
+runfile('C:/Users/Aishwarya/.spyder-py3/temp.py', wdir='C:/Users/Aishwarya/.spyder-py3')
+runfile('C:/Users/Aishwarya/.spyder-py3/untitled0.py', wdir='C:/Users/Aishwarya/.spyder-py3')
+runfile('C:/Users/Aishwarya/.spyder-py3/Covid19.py', wdir='C:/Users/Aishwarya/.spyder-py3')
+runfile('C:/Users/Aishwarya/.spyder-py3/covid19_analysis.py', wdir='C:/Users/Aishwarya/.spyder-py3')
+
+## ---(Thu Mar 23 16:29:35 2023)---
+debugfile('C:/Users/Aishwarya/.spyder-py3/covid19_analysis.py', wdir='C:/Users/Aishwarya/.spyder-py3')
---
+++ temp.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv("covid19_patient_data.csv")
+print(df)
+
+print(df.head())
+
+print(df.tail())
+
+print(df.dtypes)
+
+print(df.isnull().sum())
+print("Total number of matches: ",df['Age'].max())
+
+print("All the unique ages are: ",df['Age'].unique())
+
+print("Total no of unique ages are: ",len(df['Age'].unique()))
+
+max_bloodpressure=df['BloodPressure'].max()
+print(max_bloodpressure)
+
+print('Name of the team win by maximum runs= ',df.loc[df.BloodPressure==max_bloodpressure]['Insulin'])
+
+#seaborn package - barplot(bydefault)
+#sns.countplot(x='ThroatInfections',data=df)
+
+
+#scatter plot
+x=df['BloodPressure']
+y=df['Insulin']
+plt.scatter(x,y,color='green',cmap='Dark2',alpha=0.5)
+plt.colorbar()
+plt.show()
+
+#xpoint=df['BMI']
+#ypoint=df['Symptom']
+#plt.plot(xpoint,ypoint)
+#plt.xlabel('BMI')
+#plt.ylabel('Symptom')
+#plt.show()
+
+#xpoint=df['Age']
+#ypoint=df['Death']
+#plt.bar(xpoint)
+#plt.xlabel('Age')
+#plt.ylabel('Death')
+#plt.legend()
+#plt.show()
+
+
+
